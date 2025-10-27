917d6f85b9038bc8f8d8c4e8d00ebb34d084cde3
amee <corvidernity@gmail.com>
2025-10-27 12:26:27 +0800

    notifcations + clock
    added notifs when bars are running low
    added win and lose screen

--- Assets/scripts/NotifAlerts.cs
+++ Assets/scripts/NotifAlerts.cs
@@ -9,26 +9,27 @@ public class NotifAlerts : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI alertBox;
 
-    public bool HungerAlert(string location)
+    public bool HungerAlert(string animal)
     {
         //putting in the text
-        string alert = string.Format("<b><color=#{0}>{1} </b><color=#FFFFFF>is running low in {2}", ColorUtility.ToHtmlStringRGB(hungerColor), "FOOD BAR", location);
+        string alert = string.Format("<b><color=#{0}>{1} </b><color=#FFFFFF>is running low in {2} Enclosure", ColorUtility.ToHtmlStringRGB(hungerColor), "FOOD BAR", animal);
+        alertBox.text += "\r\n" + alert;
+
+
+        return true;
+    }
+    public bool HealthAlert(string animal)
+    {
+        //putting in the text
+        string alert = string.Format("<b><color=#{0}>{1} </b><color=#FFFFFF>is running low in {2} Enclosure", ColorUtility.ToHtmlStringRGB(healthColor), "HEALTH BAR", animal);
         alertBox.text += "\r\n" + alert;
 
         return true;
     }
-    public bool HealthAlert(string location)
+    public bool SanityAlert(string animal)
     {
         //putting in the text
-        string alert = string.Format("<b><color=#{0}>{1} </b><color=#FFFFFF>is running low in {2}", ColorUtility.ToHtmlStringRGB(healthColor), "HEALTH BAR", location);
-        alertBox.text += "\r\n" + alert;
-
-        return true;
-    }
-    public bool SanityAlert(string location)
-    {
-        //putting in the text
-        string alert = string.Format("<b><color=#{0}>{1} </b><color=#FFFFFF>is running low in {2}", ColorUtility.ToHtmlStringRGB(sanityColor), "SANITY BAR", location);
+        string alert = string.Format("<b><color=#{0}>{1} </b><color=#FFFFFF>is running low in {2} Enclosure", ColorUtility.ToHtmlStringRGB(sanityColor), "SANITY BAR", animal);
         alertBox.text += "\r\n" + alert;
 
         return true;
